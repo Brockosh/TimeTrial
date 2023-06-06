85d749a28cec72e3fc45e1929f03473857968292
Brockosh <brockjoshea@gmail.com>
2023-06-06 15:41:29 +1000

    More work completed on text prompts.

--- Assets/Scripts/MainSceneScripts/TextPrompts.cs
+++ Assets/Scripts/MainSceneScripts/TextPrompts.cs
@@ -9,17 +9,31 @@ public class TextPrompts : MonoBehaviour
     public TextMeshProUGUI findTheRightDoor;
     public TextMeshProUGUI invertedMovement;
     public TextMeshProUGUI dodgeTheGhosts;
+    public TextMeshProUGUI weirdMovement;
+    public TextMeshProUGUI moveAroundTheBrickWalls;
+    public TextMeshProUGUI finished;
+    private bool hasPlayerHitDifficultMovementPlane;
+    private bool hasPlayerHitEnemySpawningPlane;
     // Start is called before the first frame update
     void Start()
     {
         findTheRightDoor.gameObject.SetActive(false);
         invertedMovement.gameObject.SetActive(false);
         dodgeTheGhosts.gameObject.SetActive(false);
+        weirdMovement.gameObject.SetActive(false);
+        moveAroundTheBrickWalls.gameObject.SetActive(false);
+        finished.gameObject.SetActive(false);
+        hasPlayerHitDifficultMovementPlane = false;
+        hasPlayerHitEnemySpawningPlane = false;
         GameManager.instance.CollisionEvent.OnPlayerCollisionStartLine += ActivateFindTheRightDoorText;
         GameManager.instance.CollisionEvent.OnPlayerCollisionCorrectDoor += DeativateFindTheRightDoorText;
-        //GameManager.instance.CollisionEvent.OnPlayerCollisionEnemySpawningPlane += ActivateInvertedMovementAndGhostDodgeText;
         GameManager.instance.CollisionEvent.OnPlayerCollisionEnemySpawningPlane += RunEnemySpawningPlaneTextOperations;
+        GameManager.instance.CollisionEvent.OnPlayerCollisionEnemySpawningPlane += ActivateEnemySpawningBool;
         GameManager.instance.CollisionEvent.OnPlayerCollisionEnemySpawningPlaneExit += DeactivateInvertedMovementAndGhostDodgeText;
+        GameManager.instance.CollisionEvent.OnPlayerCollisionDifficultMovementPlane += RunDifficultMovementTextOperations;
+        GameManager.instance.CollisionEvent.OnPlayerCollisionDifficultMovementPlane += ActivateDifficultMovementBool;
+        GameManager.instance.CollisionEvent.OnPlayerCollisionDifficultMovementPlaneExit += DeactivateWeirdMovementAndBrickWallText;
+        GameManager.instance.CollisionEvent.OnPlayerCollisionFinishLine += ActivateFinishedText;
     }
 
     // Update is called once per frame
@@ -51,8 +65,47 @@ public class TextPrompts : MonoBehaviour
 
     private void RunEnemySpawningPlaneTextOperations()
     {
-        ActivateInvertedMovementAndGhostDodgeText();
-        Invoke("DeactivateInvertedMovementAndGhostDodgeText", 3f);
+        if (!hasPlayerHitEnemySpawningPlane)
+        {
+            ActivateInvertedMovementAndGhostDodgeText();
+            Invoke("DeactivateInvertedMovementAndGhostDodgeText", 3f);
+        }
     }
 
+    private void ActivateWeirdMovementAndBrickWallText()
+    {
+        weirdMovement.gameObject.SetActive(true);
+        moveAroundTheBrickWalls.gameObject.SetActive(true);
+    }
+
+    private void DeactivateWeirdMovementAndBrickWallText()
+    {
+        weirdMovement.gameObject.SetActive(false);
+        moveAroundTheBrickWalls.gameObject.SetActive(false);
+    }
+
+    private void RunDifficultMovementTextOperations()
+    {
+        if (!hasPlayerHitDifficultMovementPlane)
+        {
+            ActivateWeirdMovementAndBrickWallText();
+            Invoke("DeactivateWeirdMovementAndBrickWallText", 3f);
+        }
+    }
+
+    private void ActivateDifficultMovementBool()
+    {
+        hasPlayerHitDifficultMovementPlane = true;
+    }
+
+    private void ActivateEnemySpawningBool()
+    {
+        hasPlayerHitEnemySpawningPlane = true;
+    }
+
+    private void ActivateFinishedText()
+    {
+        finished.gameObject.SetActive(true);
+    }
 }
+
